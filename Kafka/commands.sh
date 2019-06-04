a7eeb85009ca79043ab7b5dfd0622f632ab3b79d
balaji <balaji.chopparapu@gmail.com>
2019-06-04 10:13:15 +0530

    init

---
+++ Kafka/commands.sh
@@ -0,0 +1,28 @@
+# Start Zookeeper
+# /home/mario/soft/kafka_2.12-2.2.0
+./bin/zookeeper-server-start.sh config/zookeeper.properties
+
+# Start kafka server
+./bin/kafka-server-start.sh config/server.properties
+
+# Data files of zookeeper and kafka
+/home/mario/soft/kafka_2.12-2.2.0/data/kafka
+/home/mario/soft/kafka_2.12-2.2.0/data/zookeeper
+
+# Create topic
+./bin/kafka-topics.sh --create --topic first_topic --zookeeper localhost:2181 --replication-factor 1 --partitions 1
+
+# Describe topic
+./bin/kafka-topics.sh --describe --topic thrid_topic --zookeeper localhost:2181
+
+#List topic
+./bin/kafka-topics.sh --list --zookeeper localhost:2181
+
+# Producing to kafka topic
+./bin/kafka-console-producer.sh \
+    --broker-list 127.0.0.1:9092 --topic first_topic
+
+# Console producer with all acks
+./bin/kafka-console-producer.sh \
+    --broker-list 127.0.0.1:9092 --topic first_topic \
+	-- producer-property acks=all0
